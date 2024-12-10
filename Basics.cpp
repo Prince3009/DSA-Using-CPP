1d8f2e23febbbbe91db3ada6a3d1957a9eb2f7d7
Prince3009 <princechourasia@gmail.com>
2024-12-10 23:23:48 +0530

    Basicss of C++

---
+++ Basics.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>             //all the libraries are included 
+using namespace std;
+int main(){
+    cout << "Prince" << endl;
+
+    //data types
+    int a = 10;
+    float b = 2.5;
+    double c = 3.56789;
+    char d = 'A';
+    bool e = true;
+    string f = "Hello, World!";
+    cout << "Integer: " << a << endl;
+    cout << "Float: " << b << endl;
+    cout << "Double: " << c << endl;
+    cout << "Character: " << d << endl;
+    cout << "Boolean: " << e << endl;               //if true return 1 and and false return 0
+
+
+    //taking user input
+    // int userInput;
+    // cout << "Enter an integer: ";
+    // cin >> userInput;
+    // cout << "You entered: " << userInput << endl;
+
+
+    //taking user input of a string
+    // string name;
+    // cout << "Enter your name: ";
+    // getline(cin, name);                               //getline will print the whole sentence
+    // cout << "Hello, " << name << endl;
+
+
+
+    //if-else statement
+    // int age;
+    // cout << "Enter your age: ";
+    // cin >> age;
+    // if(age >= 18){
+    //     cout << "You are an adult" << endl;
+    // }
+    // else if(age >= 13 && age < 18){
+    //     cout << "You are a teenager" << endl;
+    // }
+    // else{
+    //     cout << "You are a child" << endl;
+    // }
+
+
+
+//switch statement
+    // int day;
+    // cout << "Enter a day (1-7): ";  
+    // cin >> day;
+    // switch(day){
+    //     case 1:
+    //         cout << "Monday";
+    //         break;
+    //     case 2:
+    //         cout << "Tuesday";
+    //         break;
+    //     case 3:
+    //         cout << "Wednesday";
+    //         break;
+    //     case 4:
+    //         cout << "Thursday";
+    //         break;
+    //     case 5:
+    //         cout << "Friday";
+    //         break;
+    //     case 6:
+    //         cout << "Saturday";
+    //         break;
+    //     case 7:
+    //         cout << "Sunday";
+    //         break;
+    //     default:
+    //         cout << "Invalid day";
+    // }
+
+
+
+    //arrays
+    // int arr[5] = {1, 2, 3, 4, 5};
+    // for(int i = 0; i < 5; i++){
+    //     cout << arr[i] << " ";
+    // }
+
+
+
+    // array taking user inputs
+    // int arr[5];
+    // for(int i = 0; i < 5; i++){
+    //     cout << "Enter element " << i+1 << ": ";                  
+    //     cin >> arr[i];                                             //this will ask for user input everytime
+    // }
+    // cout << "[ ";
+    // for(int i = 0; i < 5; i++){
+    //     cout << arr[i] << " ";                                    //print the given input in form of array
+    // }
+    // cout << "]";
+
+
+
+    //2-D array
+    // int arr[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    // for(int i = 0; i < 3; i++){
+    //     for(int j = 0; j < 3; j++){
+    //         cout << arr[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+}
