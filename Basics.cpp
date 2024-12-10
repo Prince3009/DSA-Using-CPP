bfad82f24bff557d2f04eb739ee8282603fb3472
Prince3009 <princechourasia@gmail.com>
2024-12-10 23:31:40 +0530

    Basicss of C++

--- Basics.cpp
+++ Basics.cpp
@@ -110,4 +110,22 @@ int main(){
     //     }
     //     cout << endl;
     // }
+
+
+
+    //while loop
+    // int i = 0;
+    // while(i < 5){
+    //     cout << i << endl;
+    //     i++;
+    // }
+
+
+
+    //do-while loop - it will execute the code atleast one time for sure
+    // int i = 0;
+    // do{
+    //     cout << i << endl;
+    //     i++;
+    // }while(i < 5);
 }
