625db008da76dc83bdef16028f7dcfe2592a408d
Prince3009 <princechourasia1111@gmail.com>
2025-01-25 18:42:28 +0530

    Recursion

--- 6Recursion.cpp/3.cpp
+++ 6Recursion.cpp/3.cpp
@@ -1,17 +1,17 @@
-//Print numbers from 1 to N using recusion
+//Print numbers in reverse order 5 4 3 2 1
 #include<bits/stdc++.h>
 using namespace std;
 
 void f(int i, int n){
-    if(i > n)
+    if(i < 1)
     return;
     cout << i << endl;
-    f(i+1, n);
+    f(i-1, n);
 
 }
 
 int main(){
     int n;
     cin >> n;
-    f(1,n);
+    f(n,n);
 }
