f52c0aa2a7e97806965563d3d4f9fecd3bbdb2a6
Prince3009 <princechourasia1111@gmail.com>
2025-02-22 01:24:08 +0530

    Arrays

---
+++ 9Arrays/2Medium/8NextPermutation.cpp
@@ -0,0 +1,51 @@
+/*
+A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
+
+For example, for arr = [1,2,3], the following are all the permutations of arr: [1,2,3], [1,3,2], [2, 1, 3], [2, 3, 1], [3,1,2], [3,2,1].
+The next permutation of an array of integers is the next lexicographically greater permutation of its integer. More formally, if all the permutations of the array are sorted in one container according to their lexicographical order, then the next permutation of that array is the permutation that follows it in the sorted container. If such arrangement is not possible, the array must be rearranged as the lowest possible order (i.e., sorted in ascending order).
+
+For example, the next permutation of arr = [1,2,3] is [1,3,2].
+Similarly, the next permutation of arr = [2,3,1] is [3,1,2].
+While the next permutation of arr = [3,2,1] is [1,2,3] because [3,2,1] does not have a lexicographical larger rearrangement.
+Given an array of integers nums, find the next permutation of nums.
+
+The replacement must be in place and use only constant extra memory.
+
+Example 1:
+Input: nums = [2,1,5,4,3,0,0]
+Output: [2,3,0,0,1,4,5]
+*/
+#include<bits/stdc++.h>
+using namespace std;
+void nextPermutation(int arr[], int n){
+    int index = -1;
+    for(int i = n-2 ; i >= 0; i--){
+        if(arr[i] < arr[i+1]){
+            index = i;
+            break;
+        }
+    }
+    if(index == -1){
+        reverse(arr, arr+n);
+    }
+    for(int i = n-1; i >= index; i--){
+        if(arr[i] > arr[index]){
+            swap(arr[i], arr[index]);
+            break;
+        }
+    }
+    reverse(arr + index + 1, arr+n);
+}
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    nextPermutation(arr, n);
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
