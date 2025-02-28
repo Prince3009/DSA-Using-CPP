eb6c3870271e55e840f8f8d21be568c56e3c2f5e
Prince3009 <princechourasia1111@gmail.com>
2025-02-28 23:30:05 +0530

    Arrays

--- 9Arrays/2Medium/1LongestSubarrayWithSumK.cpp
+++ 9Arrays/2Medium/1LongestSubarrayWithSumK.cpp
@@ -37,8 +37,38 @@ using namespace std;
 
 
 //Optimal approach if the array contains negative numbers also
-int longestSubarray(int arr[], int n, int k){
+int longestSubarray(vector<int>& a, long long k) {
+    int n = a.size(); // size of the array.
+    if(n == 0) return 0;
     
+    map<long long, int> preSumMap;
+    long long sum = 0;
+    int maxLen = 0;
+    for (int i = 0; i < n; i++) {
+        //calculate the prefix sum till index i:
+        sum += a[i];
+
+        // if the sum = k, update the maxLen:
+        if (sum == k) {
+            maxLen = max(maxLen, i + 1);
+        }
+
+        // calculate the sum of remaining part i.e. x-k:
+        long long rem = sum - k;
+
+        //Calculate the length and update maxLen:
+        if (preSumMap.find(rem) != preSumMap.end()) {
+            int len = i - preSumMap[rem];
+            maxLen = max(maxLen, len);
+        }
+
+        //Finally, update the map checking the conditions:
+        if (preSumMap.find(sum) == preSumMap.end()) {
+            preSumMap[sum] = i;
+        }
+    }
+
+    return maxLen;
 }
 
 
@@ -46,11 +76,11 @@ int longestSubarray(int arr[], int n, int k){
 int main(){
     int n;
     cin >> n;
-    int arr[n];
+    vector<int> arr(n);
     for(int i = 0; i < n; i++){
         cin >>arr[i];
     }
     int k;
     cin >> k;
-    cout << longestSubarray(arr, n, k);
+    cout << longestSubarray(arr, k);
 }
