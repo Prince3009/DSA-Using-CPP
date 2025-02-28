a452c94afd9c4f3382bb8e67315c320136fb2bd2
Prince3009 <princechourasia1111@gmail.com>
2025-02-28 18:13:51 +0530

    Arrays

--- 9Arrays/2Medium/10LongestConsecutiveSequence.cpp
+++ 9Arrays/2Medium/10LongestConsecutiveSequence.cpp
@@ -1,14 +1,14 @@
 /*
-Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+Given an unsorted array of integers arr, return the length of the longest consecutive elements sequence.
 You must write an algorithm that runs in O(n) time.
 
 Example 1:
-Input: nums = [100,4,200,1,3,2]
+Input: arr = [100,4,200,1,3,2]
 Output: 4
 Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
 
 Example 2:
-Input: nums = [0,3,7,2,5,8,4,6,0,1]
+Input: arr = [0,3,7,2,5,8,4,6,0,1]
 Output: 9
 */
 #include<bits/stdc++.h>
@@ -38,24 +38,33 @@ using namespace std;
 
 
 
-//Better approach
-int longestConSequence(int arr[], int n){
-    sort(arr, arr+n);
-    int longest = 1;
+// //Better approach
+int longestConSequence(int arr[], int n)
+{
+    sort(arr, arr + n);
+    if (n == 0) return 0;
     int count = 1;
+    int longest = 1;
     int last_smaller = arr[0];
-    for(int i = 0; i < n; i++){
-        if(arr[i]-1 == last_smaller){
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] == last_smaller)
+            continue;
+
+        else if (arr[i] == last_smaller + 1)
+        {
             count++;
-            last_smaller = arr[i];
         }
-        else if(arr[i] != last_smaller){
+        else if (arr[i] != last_smaller)
+        {
             count = 1;
         }
+        last_smaller = arr[i];
         longest = max(longest, count);
     }
     return longest;
 }
+
 int main(){
     int n; 
     cin >> n;
