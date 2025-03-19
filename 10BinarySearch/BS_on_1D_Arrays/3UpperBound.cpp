f0834deca2962532ee95b059402e45850f9c75e5
Prince3009 <princechourasia1111@gmail.com>
2025-03-20 00:19:43 +0530

    BS

---
+++ 10BinarySearch/BS_on_1D_Arrays/3UpperBound.cpp
@@ -0,0 +1,29 @@
+//Smallest index such that arr[index] > target
+#include<bits/stdc++.h>
+using namespace std;
+int upperBound(int arr[], int n, int target){
+    int low = 0, high = n-1;
+    int ans = n; //If the target value is greater than every elememt then it will return the size
+    while(low <= high){
+        int mid = (low+high)/2;
+        if(arr[mid] > target){
+            ans = mid;
+            high = mid - 1;
+        }
+        else{
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
+int main(){
+    int n; 
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    int target;
+    cin >> target;
+    cout << upperBound(arr, n, target);
+}
